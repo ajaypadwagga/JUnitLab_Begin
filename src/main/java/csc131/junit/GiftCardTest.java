89e167c9c2f7b0d7869348820c7d62d95789fdb1
unknown <Ajay Padwagga>
2021-03-13 04:20:47 -0800

    This is the completed lab. Committing final draft.

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -1,8 +1,11 @@
 package csc131.junit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+//import org.junit.rules.ExpectedException;
+
 
 public class GiftCardTest {
 
@@ -42,16 +45,78 @@ public class GiftCardTest {
 		double       balance;
 		GiftCard     card; 
 		int          issuingStore;
-		String		 d;
+		String		 s;
 		
 		issuingStore = 1337; 
 		balance      = 100.00;
 		card = new GiftCard(issuingStore, balance);
 		
-		d = "Remaining Balance: " + String.format("%6.2f", 90.00);
-		System.out.println(d);
-		assertEquals("deduct(10.00)", d, card.deduct(10.0));
+		s = "Remaining Balance: " + String.format("%6.2f", 90.00);
+		System.out.println(s);
+		assertEquals("deduct(10.00)", s, card.deduct(10.0));
 		
 	}
-
+	
+	@Test
+	public void deduct_AmountDue()
+	{
+		double       balance;
+		GiftCard     card; 
+		int          issuingStore;
+		String		 s;
+		
+		issuingStore = 1337; 
+		balance      = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		s = "Amount Due: " + String.format("%6.2f", 10.00);
+		System.out.println(s);
+		assertEquals("deduct 110.00 from 100.00", s, card.deduct(110.0));
+	}
+	
+	@Test
+	public void deduct_InvalidTransaction()
+	{
+		double       balance;
+		GiftCard     card; 
+		int          issuingStore;
+		String		 s;
+		
+		issuingStore = 1337; 
+		balance      = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		s = "Invalid Transaction";
+		assertEquals("deduct -10.00 from 100.00", s, card.deduct(-10.0));
+	}
+	
+	@Test
+	public void testExc()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+	}
+	
+	//(expected = IllegalArgumentException.class)
+	//public void constructor_IncorrectBalance() throws IllegalArgumentException
+	//{
+	//	new GiftCard(1, -100.00);
+	//}
+	//assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+	
+	
+	@Test
+	public void constructor_IncorrectID_Low()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-1,100.00);});
+		//new GiftCard(-1, 100.00);
+	}
+	
+	@Test
+	public void constructor_IncorrectID_High()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(100000000,100.00);});
+		//new GiftCard(100000, 100.00);
+	}
+	
+	
 }
